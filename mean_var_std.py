488dc8ce554c30f9bf6aec6ab7c4627965535cad
Derek Mudie <delm83@googlemail.com>
2024-07-15 17:52:44 +0000

    completed project

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,15 @@
 import numpy as np
 
 def calculate(list):
-
-
-
-
+    if len(list) < 9:
+        raise ValueError('List must contain nine numbers.')
+    numparray = np.array(list)
+    numpmatrix = numparray.reshape(3, 3)
+    mean = [numpmatrix.mean(axis=0).tolist(), numpmatrix.mean(axis=1).tolist(), numpmatrix.mean().tolist()]
+    var = [numpmatrix.var(axis=0).tolist(), numpmatrix.var(axis=1).tolist(), numpmatrix.var().tolist()]
+    stddeviation = [numpmatrix.std(axis=0).tolist(), numpmatrix.std(axis=1).tolist(), numpmatrix.std().tolist()]
+    max = [numpmatrix.max(axis=0).tolist(), numpmatrix.max(axis=1).tolist(), numpmatrix.max().tolist()]
+    min = [numpmatrix.min(axis=0).tolist(), numpmatrix.min(axis=1).tolist(), numpmatrix.min().tolist()] 
+    sum = [numpmatrix.sum(axis=0).tolist(), numpmatrix.sum(axis=1).tolist(), numpmatrix.sum().tolist()] 
+    calculations = {'mean': mean, 'variance': var, 'standard deviation': stddeviation, 'max': max, 'min': min, 'sum': sum}
     return calculations
